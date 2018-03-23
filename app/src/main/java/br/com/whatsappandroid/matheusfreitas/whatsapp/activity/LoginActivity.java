4cc21d4ccc3fc578cb12c4e425412f4056d948b6
victormf123 <victormf.df@hotmail.com>
2018-03-23 09:20:34 -0300

    First Commit

---
+++ app/src/main/java/br/com/whatsappandroid/matheusfreitas/whatsapp/activity/LoginActivity.java
@@ -0,0 +1,142 @@
+package br.com.whatsappandroid.matheusfreitas.whatsapp.activity;
+
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.github.rtoshiro.util.format.SimpleMaskFormatter;
+import com.github.rtoshiro.util.format.text.MaskTextWatcher;
+
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Random;
+
+import br.com.whatsappandroid.matheusfreitas.whatsapp.R;
+import br.com.whatsappandroid.matheusfreitas.whatsapp.helper.Permissao;
+import br.com.whatsappandroid.matheusfreitas.whatsapp.helper.Preferencias;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText nome;
+    private EditText telefone;
+    private EditText codPais;
+    private EditText codArea;
+    private Button cadastrar;
+    private String[] permissoesNecessarias = new String[]{
+            Manifest.permission.SEND_SMS,
+            Manifest.permission.INTERNET
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Permissao.validaPermissoes(1, this, permissoesNecessarias);
+
+        nome = (EditText) findViewById(R.id.edit_nome);
+        telefone = (EditText) findViewById(R.id.edit_telefone);
+        codPais = (EditText) findViewById(R.id.edit_cod_pais);
+        codArea = (EditText) findViewById(R.id.edit_cod_area);
+        cadastrar = (Button) findViewById(R.id.bt_cadastrar);
+
+        /*Definir mascaras*/
+        SimpleMaskFormatter simpleMaskCodPais = new SimpleMaskFormatter("+NN");
+        SimpleMaskFormatter simpleMaskCodArea = new SimpleMaskFormatter("NN");
+        SimpleMaskFormatter simpleMaskTelefone = new SimpleMaskFormatter("NNNNN-NNNN");
+
+        MaskTextWatcher maskCodArea = new MaskTextWatcher(codArea, simpleMaskCodArea);
+        MaskTextWatcher maskCodPais = new MaskTextWatcher(codPais, simpleMaskCodPais);
+        MaskTextWatcher maskTelefone = new MaskTextWatcher(telefone, simpleMaskTelefone);
+
+        telefone.addTextChangedListener(maskTelefone);
+        codPais.addTextChangedListener(maskCodPais);
+        codArea.addTextChangedListener(maskCodArea);
+
+        cadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nomeUsuario = nome.getText().toString();
+                String telefoneCompleto = codPais.getText().toString() + codArea.getText().toString() + telefone.getText().toString();
+                String telefoneSemFormatacao = telefoneCompleto.replace("+", "");
+                telefoneSemFormatacao = telefoneSemFormatacao.replace("-", "");
+
+
+
+                //Gerar token
+                Random randomico = new Random();
+                int numeroRandomico = randomico.nextInt(9999 - 1000) + 1000;
+
+                String token = String.valueOf(numeroRandomico);
+                Preferencias preferencias = new Preferencias(getApplicationContext());
+
+                preferencias.slavarUsuarioPreferencias(nomeUsuario, telefoneSemFormatacao, token);
+                HashMap<String,String> usuario = preferencias.getDadosUsuario();
+
+                //Log.i("TOKEN", "NOME: " + usuario.get("nome") + " FONE: " + usuario.get("telefone"));
+
+                telefoneSemFormatacao = "8135";
+                String mensagemEnvio = "WhatsApp Código de confirmação: "+ token;
+                //Envio do SMS
+                boolean envioSMS = enviaSMS("+" + telefoneSemFormatacao,  mensagemEnvio);
+
+                if(envioSMS){
+                    Intent intent = new Intent(LoginActivity.this, ValidadorActivity.class);
+                    startActivity(intent);
+                    finish();
+                }else {
+                    Toast.makeText(LoginActivity.this, "Problema ao enviar o SMS, tente novamente!!", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    /*Envio do SMS*/
+
+    private boolean enviaSMS(String telefone, String mensagem){
+        try{
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage(telefone, null, mensagem, null, null);
+            return true;
+        }catch (Exception e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults){
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        for(int resultado: grantResults){
+            if(resultado == PackageManager.PERMISSION_DENIED){
+                alertaValidacaoPermissao();
+            }
+        }
+    }
+
+    private void alertaValidacaoPermissao(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Permissões negadas");
+        builder.setMessage("Para utilizar esse app, é necessário aceitar as permisões");
+
+        builder.setPositiveButton("CONFIRMAR", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+}
---
+++ app/src/main/java/br/com/whatsappandroid/matheusfreitas/whatsapp/activity/MainActivity.java
@@ -0,0 +1,22 @@
+package br.com.whatsappandroid.matheusfreitas.whatsapp.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import br.com.whatsappandroid.matheusfreitas.whatsapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    //private DatabaseReference referenciaFirebase = FirebaseDatabase.getInstance().getReference();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //referenciaFirebase.child("pontos").setValue(100);
+    }
+}
